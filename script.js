7e5b0b6009a06d7edf1a00ac15048400cd2df034
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-03 13:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let cost = 0;
+	var newarr = arr.sort((a,b)=>a-b);
+	while(newarr.length>1){
+		let first = newarr.shift();
+		let second =  newarr.shift();
+		let sum = first+second;
+		cost += sum;
+		newarr.push(sum);
+		newarr.sort((a,b)=>a-b);
+	}
+	return cost;
   
 }
 
